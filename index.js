57213036190567d8abae39d6c63a9708b42d2442
Seb Gertz <sebgertz@gmail.com>
2018-05-10 15:35:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ function lowerCaseDrivers(drivers) {
   return drivers.map(function(driverName){return driverName.toLowerCase() });
 }
 
-function nameToAttributes(drivers) {
-  const driversObjects = drivers.map(function(driver) { })
+function splitName(names) {
+  return names.map(function(name) {
+    return name.split(" ");
+  })
+}
+
+const splitNames = splitName(drivers);
+
+function objectConstructor(splitNames) {
+  const driversObjects= [];
+  splitNames.map(function(splitName) {
+    const newObj = {};
+    newObj.firstName = splitName[0];
+    newObj.secondName = splitName[1];
+    return driversObjects.push(newObj);
+  })
+  return driversObjects;
 }
